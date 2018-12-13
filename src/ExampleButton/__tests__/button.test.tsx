2f752c789b7203f317a97b2843923231b48b4ae7
Devin Clark <devin.clark27@gmail.com>
2018-12-13 14:16:06 -0600

    Removing tests and react-scripts

--- src/ExampleButton/__tests__/button.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { ExampleButton } from '../button';
-
-it('renders without crashing', () => {
-  shallow(<ExampleButton text='hey' />);
-});
