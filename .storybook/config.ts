6a2fa2927fd88c52f3e7bec30c33009967b6431f
Devin Clark <devin.clark27@gmail.com>
2019-04-08 16:22:24 -0500

    Upgrading to storybook 5, addressing breaking changes

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -2,7 +2,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withKnobs } from '@storybook/addon-knobs';
 
-const req = require.context('../src/stories', true, /.stories.tsx$/);
+const req = require.context('../stories', true, /.stories.tsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
 import { withInfo } from '@storybook/addon-info';
-import { ExampleButton } from '../ExampleButton/button';
+import { ExampleButton } from '../src/ExampleButton/button';
 
 const stories = storiesOf('Button', module);
 
